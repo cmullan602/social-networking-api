6e93cf7ca9b5df22397944296772a4181bd6edbf
Connor Mullan <cmullan602@gmail.com>
2022-10-09 20:08:12 -0700

    changed name of db.

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/developersApplications', {
+connect('mongodb://localhost/socialNetworkingApi', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
